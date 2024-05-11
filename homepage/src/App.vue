26d3b2acf019e49bc256e5f110d946d22619c628
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-05-11 17:20:36 -0500

    added the search bar

--- homepage/src/App.vue
+++ homepage/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
-
+import searchBar from './components/searchBar.vue'
 </script>
 
 <template>
-  
+  <div class="flex flex-col justify-center align-middle">
+  <searchBar />
+  </div>
 </template>
 
 <style scoped>
