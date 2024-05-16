f437cafe204f7f7bdab0f09d9a30b6934300040a
Terminal123 <138257699+Dark-Ai-py@users.noreply.github.com>
2024-05-16 18:15:06 -0500

    tryed to add localstorage

--- homepage/src/App.vue
+++ homepage/src/App.vue
@@ -3,7 +3,9 @@ import searchBar from './components/searchBar.vue'
 import quickLink from './components/quickLink.vue';
 import { ref } from 'vue';
 
-const links = [{
+
+
+const links = ref([{
   title: "youtube",
   description: "videos",
   logo: "https://m.youtube.com/static/apple-touch-icon-114x114-precomposed.png",
@@ -15,7 +17,37 @@ const links = [{
   title: "youtube",
   description: "videos",
   logo: "https://m.youtube.com/static/apple-touch-icon-114x114-precomposed.png",
-},]
+},])
+function updateLinks() {
+  for(let i; numOfLinks() >= i; i++) {
+    links.push(getLink[i]);
+  }
+}
+
+function addLink(newlink) {
+  let currentLinks = localStorage.getItem('links');
+  currentLinks = JSON.parse(currentLinks);
+  let newLinks = JSON.stringify([...currentLinks, {
+    title: newlink.title,
+    description: newlink.description,
+    logo: newlink.logo
+  }]);
+  localStorage.setItem('links', newLinks);
+}
+
+function getLink(i) {
+  let rawLinks = localStorage.getItem('links');
+  rawLinks = JSON.parse(rawLinks)
+  let cleanLinks = [...rawLinks];
+  return cleanLinks[i];
+}
+
+function numOfLinks() {
+  let rawLinks = localStorage.getItem('links');
+  rawLinks = JSON.parse(rawLinks)
+  let cleanLinks = [...rawLinks];
+  return cleanLinks.length;
+}
 
 const theme = 'dim'
 
@@ -27,6 +59,7 @@ const theme = 'dim'
   <div class="flex space-x-2 flex-wrap w-4/5 justify-center">
     <quickLink v-for="(link, index) in links" :key="index" :title="link.title" :description="link.description" :logo="link.logo" />    
   </div>
+    <button class="btn" @click.prevent="addLink(links[1]), updateLinks">addlink</button>
   </div>
 </template>
 
--- homepage/src/components/quickLink.vue
+++ homepage/src/components/quickLink.vue
@@ -9,16 +9,18 @@ function click() {
 </script>
 
 <template>
-    <button class="btn w-32 h-36 flex justify-center items-center">
-        <div class="avatar">
-            <div class="w-24 rounded">
+    <button class="btn w-fit h-fit flex justify-center items-center flex-col gap-0 pr-2 pl-2">
+        <div class="avatar pt-2">
+            <div class="w-24 h-24 rounded">
                 <img :src="logo" />
             </div>
         </div>
-        <span class="badge">{{ title }}</span>
+        <span class="badge mt-2 mb-1">{{ title }}</span>
     </button>
 </template>
 
 <style scoped>
-
+    .h-38 {
+        height: 9.5rem;
+    }
 </style>
